96a856294767785d2b246539c499fea5cad16e4b
nguyenngocbang <bang270197@gmail.com>
2019-03-29 20:59:32 +0700

    if - else

---
+++ luyentapvoiifelse/ifelse.js
@@ -0,0 +1,12 @@
+let chuoi =prompt("What is the “official” name of JavaScript?");
+if(chuoi=="")
+{
+    alert("Nhap gi do!");
+}else{
+    if(chuoi=="ECMAScript"){
+        alert("Right!");
+    }else
+    {
+        alert("Didn’t know? ECMAScript!");
+    }
+}
